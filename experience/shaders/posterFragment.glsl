8e979866d6efc443360492131feceb3dc0d168f8
sujen <sujenphea@gmail.com>
2022-09-21 08:10:49 +1200

    feat: mouse interaction affecting brightness

--- experience/shaders/posterFragment.glsl
+++ experience/shaders/posterFragment.glsl
@@ -6,7 +6,8 @@ void main() {
     vec3 color = vec3(1.0, 0.5, 0.5);
     vec2 st = vUv;
 
-    color = vec3(uMouse, 0.0);
+    // color = vec3(uMouse, 0.0);
+    color *= 1.0 - length(uMouse - vUv);
 
     gl_FragColor = vec4(color, 1.0);
 }
