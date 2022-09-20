8696992b09778527582c78ff79c2a7b5430d35bf
sujen <sujenphea@gmail.com>
2022-09-21 08:17:19 +1200

    feat: add image as texture

--- experience/models/Poster.tsx
+++ experience/models/Poster.tsx
@@ -1,7 +1,13 @@
 import { useEffect, useRef } from 'react'
 
-import { ShaderMaterial } from 'three'
-import { extend, Object3DNode, useFrame, useThree } from '@react-three/fiber'
+import { ShaderMaterial, TextureLoader } from 'three'
+import {
+  extend,
+  Object3DNode,
+  useFrame,
+  useLoader,
+  useThree,
+} from '@react-three/fiber'
 import { shaderMaterial } from '@react-three/drei'
 
 import fragmentShader from '../shaders/posterFragment.glsl'
@@ -10,6 +16,7 @@ import vertexShader from '../shaders/posterVertex.glsl'
 // material
 const ProjectShaderMaterial = shaderMaterial(
   {
+    uTexture: null,
     uMouse: [1.0, 1.0],
   },
   vertexShader,
@@ -27,16 +34,25 @@ declare global {
 }
 
 const Poster = () => {
+  // refs
   const shaderRef = useRef<ShaderMaterial>(null!)
 
+  // hooks
   const { viewport } = useThree()
+  const image = useLoader(TextureLoader, './testImage.png')
 
+  // tick
   useFrame(({ mouse }) => {
     const x = (mouse.x * viewport.width) / 4 + 0.5
     const y = (mouse.y * viewport.height) / 4 + 0.5
 
     shaderRef.current.uniforms.uMouse = { value: [x, y] }
   })
+
+  // effects
+  useEffect(() => {
+    shaderRef.current.uniforms.uTexture = { value: image }
+  }, [])
 
   return (
     <mesh>
--- experience/shaders/posterFragment.glsl
+++ experience/shaders/posterFragment.glsl
@@ -1,3 +1,4 @@
+uniform sampler2D uTexture;
 uniform vec2 uMouse;
 
 varying vec2 vUv;
@@ -6,8 +7,11 @@ void main() {
     vec3 color = vec3(1.0, 0.5, 0.5);
     vec2 st = vUv;
 
-    // color = vec3(uMouse, 0.0);
-    color *= 1.0 - length(uMouse - vUv);
+    vec4 textureColor = texture2D(uTexture, vUv);
+
+
+    color = textureColor.xyz;
+    color *= 1.0 - length(uMouse - st);
 
     gl_FragColor = vec4(color, 1.0);
 }
