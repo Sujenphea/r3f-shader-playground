e2aab2b6c26a834c41697b8fa07cdbde1039fead
sujen <sujenphea@gmail.com>
2022-09-21 08:09:59 +1200

    feat: mouse interaction w/ color

--- experience/models/Poster.tsx
+++ experience/models/Poster.tsx
@@ -1,14 +1,20 @@
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 
 import { ShaderMaterial } from 'three'
-import { extend, Object3DNode } from '@react-three/fiber'
+import { extend, Object3DNode, useFrame, useThree } from '@react-three/fiber'
 import { shaderMaterial } from '@react-three/drei'
 
 import fragmentShader from '../shaders/posterFragment.glsl'
 import vertexShader from '../shaders/posterVertex.glsl'
 
 // material
-const ProjectShaderMaterial = shaderMaterial({}, vertexShader, fragmentShader)
+const ProjectShaderMaterial = shaderMaterial(
+  {
+    uMouse: [1.0, 1.0],
+  },
+  vertexShader,
+  fragmentShader
+)
 
 extend({ ProjectShaderMaterial })
 
@@ -22,6 +28,15 @@ declare global {
 
 const Poster = () => {
   const shaderRef = useRef<ShaderMaterial>(null!)
+
+  const { viewport } = useThree()
+
+  useFrame(({ mouse }) => {
+    const x = (mouse.x * viewport.width) / 4 + 0.5
+    const y = (mouse.y * viewport.height) / 4 + 0.5
+
+    shaderRef.current.uniforms.uMouse = { value: [x, y] }
+  })
 
   return (
     <mesh>
--- experience/shaders/posterFragment.glsl
+++ experience/shaders/posterFragment.glsl
@@ -1,5 +1,12 @@
+uniform vec2 uMouse;
+
+varying vec2 vUv;
+
 void main() {
     vec3 color = vec3(1.0, 0.5, 0.5);
+    vec2 st = vUv;
+
+    color = vec3(uMouse, 0.0);
 
     gl_FragColor = vec4(color, 1.0);
 }
