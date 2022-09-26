96412549a64be41aa36a0cf62e0976fbe75405d3
sujen <sujenphea@gmail.com>
2022-09-26 13:27:43 +1300

    feat: hover zoom done

--- experience/experiments/hoverDistortion/hoverDistortionFragment.glsl
+++ experience/experiments/hoverDistortion/hoverDistortionFragment.glsl
@@ -3,13 +3,21 @@ uniform vec2 uMouse;
 
 varying vec2 vUv;
 
-void main() {
-  float dist = distance(vUv, uMouse) * 10.0;
-  
-  vec4 texture = texture2D(uTexture, vUv);
-  vec3 color = texture.xyz;
-  
-  float opacity = clamp(dist, 0.5, 1.0);
+float linearConvert(float value, float min1, float max1, float min2, float max2) {
+  return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
+}
 
-  gl_FragColor = vec4(color, opacity);
+void main() {
+  float dist = distance(vUv, uMouse);
+  float power = 0.08;
+  
+  float prox = 1.0 - linearConvert(dist, 0.0, 0.5, 0.0,  1.0);
+	prox = clamp(prox, 0.0, 1.0);
+
+  vec2 zoomedUV = mix(vUv, uMouse, prox * power);
+
+  vec4 texture = texture2D(uTexture, zoomedUV);
+  vec3 color = texture.xyz;
+
+  gl_FragColor = vec4(color, 1.0);
 }
