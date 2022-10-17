909856dd29e816c18fa0281802566e7acd6f0425
sujen <sujenphea@gmail.com>
2022-10-17 16:44:37 +1300

    feat: animate scale

--- experience/experiments/scrollSpheres/scrollSpheres.tsx
+++ experience/experiments/scrollSpheres/scrollSpheres.tsx
@@ -3,7 +3,7 @@ import { useEffect, useRef } from 'react'
 import { Mesh, MeshStandardMaterial, SphereGeometry, Vector3 } from 'three'
 import { useFrame } from '@react-three/fiber'
 
-import data, { interpolateVectors } from './data'
+import data, { interpolateValues, interpolateVectors } from './data'
 
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
@@ -19,14 +19,22 @@ const ScrollSpheres = () => {
     new Array(data[0].spheresData.length).fill(null)
   )
   const positionsRef = useRef<Vector3[]>([])
+  const scalesRef = useRef<number[]>([])
 
   // hooks
   useEffect(() => {
+    // setup values
     const positionArray = Array(13).fill([])
     data[0].spheresData.forEach((data, i) => {
       positionArray[i] = data.positions[0]
     })
     positionsRef.current = positionArray
+
+    const scaleArray = Array(13).fill([])
+    data[0].spheresData.forEach((data, i) => {
+      scaleArray[i] = data.scales[0]
+    })
+    scalesRef.current = scaleArray
 
     // manage scroll
     gsap.timeline({
@@ -39,17 +47,22 @@ const ScrollSpheres = () => {
           const sectionIndex = Math.min(Math.trunc(self.progress * 6), 5) // which section
           const progress = (self.progress - sectionIndex * (1 / 6)) * 6 //
 
-          const newPositionArray = Array(13).fill([])
+          // update position
           data[sectionIndex].spheresData.forEach((sphereData, i) => {
             const newPosition = interpolateVectors(
               progress,
               sphereData.positions
             )
 
-            newPositionArray[i] = newPosition
+            positionsRef.current[i] = newPosition
           })
 
-          positionsRef.current = newPositionArray
+          // update scale
+          data[sectionIndex].spheresData.forEach((sphereData, i) => {
+            const newScale = interpolateValues(progress, sphereData.scales)
+
+            scalesRef.current[i] = newScale
+          })
         },
       },
     })
@@ -58,6 +71,10 @@ const ScrollSpheres = () => {
   useFrame(() => {
     positionsRef.current.forEach((position, i) => {
       spheresRef.current[i].position.copy(position)
+    })
+
+    scalesRef.current.forEach((scale, i) => {
+      spheresRef.current[i].scale.set(scale, scale, scale)
     })
   })
 
